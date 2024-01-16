6a6a114fa14bf59ea50616d50c5a316285ed7de0
pyaephyohein <pyaephyohein.info.3326@gmail.com>
2024-01-16 20:47:30 +0630

    only body

--- variables.tf
+++ variables.tf
@@ -11,28 +11,7 @@ variable "enabled_ingress" {
   type        = bool
   default     = true
 }
-variable "enabled_loki_stack" {
-  description = "enabled loki-stack"
-  type        = bool
-  default     = false
-}
 variable "ingress_class_name" {
   description = "ingress class name"
   type        = string
-}
-variable "storage_class_name" {
-  description = "storage class name"
-  type        = string
-}
-
-variable "enabled_istio" {
-  description = "istio service mesh class name"
-  type        = string
-}
-variable "letsencrypt-email" {
-  description = "letsencrypt-email"
-}
-
-variable "letsencrypt-env" {
-  description = "letsencrypt-email"
 }
